54519c3b9d6d14a218d3ff8fbdb8661ecc3d6958
Aaron <aaronchallinor@gmail.com>
2023-05-27 10:25:19 +0100

    Messed with container sizing, icon colours and hover over to remove class

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleClass() {
+    var element = document.getElementById("disc_icon");
+    element.classList.toggle("fa-beat");
+  }
+
+  function toggleClass2() {
+    var element = document.getElementById("yt_icon");
+    element.classList.toggle("fa-beat");
+  }
